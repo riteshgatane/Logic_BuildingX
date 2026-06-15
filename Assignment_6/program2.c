4436cdff71dc0271eebce62f30661aced6f9105a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-15 13:03:52 +0530

    Add files via upload

---
+++ Assignment_6/program2.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int Display(int iNo)
+{
+   int  iCnt = 0 ;
+   if (iNo < 0)
+   {
+        iNo = -iNo ;
+   }
+   if(iNo == 1)
+   {
+     printf("One");
+   }
+   else if(iNo == 2)
+   {
+     printf("Two");
+   }
+    else if(iNo == 3)
+   {
+     printf("Three");
+   }
+    else if(iNo == 4)
+   {
+     printf("Four");
+   }
+    else if(iNo == 5)
+   {
+     printf("Five");
+   }
+    else if(iNo == 6)
+   {
+     printf("Six");
+   }
+    else if(iNo == 7)
+   {
+     printf("Seven");
+   }
+    else if(iNo == 8)
+   {
+     printf("Eight");
+   }
+    else if(iNo == 9)
+   {
+     printf("Nine");
+   }
+   else
+   {
+     printf("Invalid Number");
+   }
+   
+    
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter The Number:");
+    scanf("%d",&iValue);
+
+
+   Display(iValue);
+    
+
+    return 0;
+}
