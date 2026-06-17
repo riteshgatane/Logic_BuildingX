0289c80348ac0a06b344fb3f6d9d2f7ea4d5097a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-18 00:07:35 +0530

    Add files via upload

--- Assignment_8/program1.c
+++ Assignment_8/program1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+double CircleArea(float fRadius)
+{
+    double Area = 0;
+    float PI = 3.14 ;
+
+    Area = PI * fRadius * fRadius;
+    return Area; 
+}
+
+int main()
+{
+    float fValue = 0.0f;
+    double dRet = 0.0 ; 
+
+    printf("Enter the radius:");
+    scanf("%f",&fValue);
+
+    dRet = CircleArea(fValue);
+
+    printf("Area of the Circle is %lf10.4",dRet);
+    return 0 ;
+}
