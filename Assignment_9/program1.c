f0e434fbf91696a9464d9b9c35847bc129c83ae2
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-19 16:04:38 +0530

    Addded program1.c

--- Assignment_9/program1.c
+++ Assignment_9/program1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int CountEven(int iNo)
+{
+    int iCnt =0 ;
+    int iDigit =0 ;
+
+    while (iNo != 0 )
+    {
+        iDigit = iNo % 10 ;
+        if((iDigit%2 )== 0)
+        {
+            iCnt++ ;
+        }
+        iNo =iNo /10 ;
+    }
+
+    return iCnt ;
+    
+
+   
+}
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the number:");
+    scanf("%d",&iValue);
+
+    iRet = CountEven(iValue);
+    printf("Total even Number int the Number is:%d",iRet);
+    return 0;
+}
