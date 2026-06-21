156be90f37208c6388516c348facfd094ef46a84
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-21 06:58:03 +0530

    Add files via upload

--- Assignment_11/program11_1.c
+++ Assignment_11/program11_1.c
@@ -1,1 +1,34 @@
+////////////////////////////////////////////////////////
+//Dispaly all the number Starting point to the Ending point 
+//
+///////////////////////////////////////////////////////
 
+
+#include <stdio.h>
+void RangeDisplay(int iStart ,int iEnd)
+{
+    int iCnt =0;
+
+    for(iCnt = iStart ; iCnt <= iEnd ; iCnt ++)
+    {
+        printf("%d\t",iCnt);
+    }
+    
+
+}
+
+int main()
+{
+    int iValue1= 0 ,iValue2 =0;
+
+    printf("Enter the Starting point:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Ending point:");
+    scanf("%d",&iValue2);
+
+    RangeDisplay(iValue1,iValue2);
+
+
+    return 0;
+}
