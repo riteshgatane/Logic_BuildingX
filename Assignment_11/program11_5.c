097629534db2b8fc52066c47a144a75865a9a271
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-21 23:47:45 +0530

    Add files via upload

---
+++ Assignment_11/program11_5.c
@@ -0,0 +1,45 @@
+////////////////////////////////////////////////////////////////////////
+// Display all the Numbers Between the range in the Reverse order 
+//
+///////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+int RangeSum(int iStart ,int iEnd)
+{
+    int iCnt =0;
+    int iSum =0;
+    if(iStart > 0 && iEnd > 0)
+    {
+    for(iCnt = iEnd ;iCnt >= iStart ; iCnt--)
+    {
+        {
+            printf("%d\t",iCnt);
+            iSum =iSum +iCnt ;
+        }
+    }
+    }
+    else 
+    {
+        printf("INVALID NUMBER");
+    }
+    return iSum ;
+}
+
+int main()
+{
+    int iValue1 =0, iValue2 =0;
+    int iRet =0;
+
+    
+    printf("Enter the Starting point:");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Ending point:");
+    scanf("%d",&iValue2);
+
+    iRet = RangeSum(iValue1,iValue2);
+    printf("The Summation is:%d",iRet);
+
+    return 0;
+
+}
