3fbe9afae5de9f56467671725e9b46b05c99a53f
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-22 19:34:19 +0530

    Add files via upload

---
+++ Assignment_12/program12_4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int CountFour(int iNo)
+{
+    int iDigit =0;
+    int iCount= 0;
+
+    
+    while (iNo > 0)
+    {
+        iDigit = iNo % 10 ;
+        if(iDigit  == 4)
+        {
+            iCount++;
+        }
+        iNo = iNo /10 ;
+    }
+    return iCount ;
+}
+
+int main()
+{
+    int iValue =0;
+    int iRet =0;
+
+    printf("Enter the Number : ");
+    scanf("%d",&iValue);
+
+    iRet = CountFour(iValue);
+    printf("%d",iRet);
+
+    return 0;
+}
