759f6bec2aca5337cc76616f60af85d026dc622e
ritesh_gatane <riteshgatane2006@gmail.com>
2026-06-28 18:11:38 +0530

    Add files via upload

--- Assignment_14/program14_1.c
+++ Assignment_14/program14_1.c
@@ -1,1 +1,50 @@
+#include<stdio.h>
+#include <stdlib.h>
 
+int CountEven(int Arr[], int iLength)
+{
+    int iCnt =0 ;
+    int iCount =0 ;
+    for(iCnt = 0 ;iCnt <= iLength ; iCnt++)
+    {
+        if((Arr[iCnt]%2 ) == 0 )
+        {
+            iCount++ ;
+        }
+    }
+    return iCount ;
+}
+
+int main()
+{
+    int iSize = 0, iRet =0, iCnt =0;
+    int iLength =0 ;
+    int *p = NULL ;
+
+    printf("Enter Number of elements:");
+    scanf("%d",&iSize);
+
+    p = (int *) malloc (iSize * sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate Memory");
+        return -1 ;
+    }
+
+    printf("Enter the elements");
+
+
+    for(iCnt = 0 ; iCnt < iSize ;iCnt++)
+    {
+        printf("Enter elements  %d:",iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = CountEven(p , iSize);
+
+    printf("Frequency of Even Number is %d",iRet);
+
+    free(p);
+    return 0;
+}
