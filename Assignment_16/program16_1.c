fdfcdcac9cdd8ab7b0a1003c7f523db94435ac87
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-08 13:02:05 +0530

    Add files via upload

--- Assignment_16/program16_1.c
+++ Assignment_16/program16_1.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
 
+#define TRUE 1
+#define FALSE 0 
+
+typedef int BOOL ;
+
+int Maximum (int Arr[] , int ilength )
+{
+    int i = 0 ; 
+    int iLargest = Arr[0] ;
+    for(i= 0; i < ilength ; i++)
+    {
+        if (Arr[i] > iLargest )
+        {
+            iLargest = Arr[i] ;
+        }
+    }
+    return iLargest ;
+}
+
+int main()
+{
+    int iSize = 0 , iRet = 0 ,iCnt =  0 ;
+    int *p = NULL; //int p[];
+
+    printf("Enter The Number oF Elements : ");
+    scanf("%d",&iSize);
+
+    p= (int *)malloc(iSize * sizeof(int));
+
+    printf("Enter %d Elements\n",iSize);
+    for(iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        printf("Enter %d element :\n",iCnt+1);
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Maximum(p,iSize);
+
+    printf("Largest Number is %d", iRet);
+
+    free(p);
+
+    
+    return 0 ;
+}
