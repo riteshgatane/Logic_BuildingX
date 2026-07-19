17b0ad0faac4e06be3ffb12ca8482e4617e69697
ritesh_gatane <riteshgatane2006@gmail.com>
2026-07-19 06:40:07 +0530

    Add files via upload

---
+++ Assignment_18/program2.java
@@ -0,0 +1,26 @@
+class Logic 
+{
+    void printEvennumbers(int n)
+    {
+        int i =0 ;
+        int Number =0;
+        
+            for(i=1 ; i <= n ; i++)
+            {
+                if(i%2 == 0)
+                {
+                    System.out.println(i);
+                }
+            }
+    }
+}
+
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.printEvennumbers(12);
+    }
+}
