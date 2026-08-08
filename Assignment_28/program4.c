daf7fc68ae9c09a6cff57a778872a0a550b6ddb1
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-08 23:43:28 +0530

    Add files via upload

---
+++ Assignment_28/program4.c
@@ -0,0 +1,58 @@
+/* Inupt : iRow = 0 iCol =6 
+    Output : *  *   *   *   *   *
+             *  #   #   #   *   *
+             *  #   #   *   $   *
+             *  #   *   $   $   *
+             *  *   $   $   $   *
+             *  *   *   *   *   *
+
+*/
+
+
+#include <stdio.h>
+
+void Pattern(int iRow , int iCol)
+{
+    int i = 0 ; 
+    int j = 0  ;
+
+    for(i =1 ; i <= iRow ; i++)
+    {
+        for(j=1 ; j <= iCol ;j++)
+        {
+            if(i ==1 || i==iRow || j ==1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else if (i+j == iCol+1)
+            {
+                printf("*\t");
+            }
+            else if(i+j <= iCol)
+            {
+                printf("#\t");
+            }
+            else 
+            {
+                printf("$\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 =0  ;
+
+    printf("Enter the Number of Rows :");
+    scanf("%d",&iValue1);
+
+    printf("Enter the Number of Columns :");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1 , iValue2);
+
+    return 0 ; 
+}
