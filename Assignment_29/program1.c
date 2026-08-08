774e45df7a2f6e156a861c06a9fecfd1d10cd5cd
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-09 00:40:17 +0530

    Add files via upload

--- Assignment_29/program1.c
+++ Assignment_29/program1.c
@@ -1,1 +1,48 @@
+#include <stdio.h>
 
+#define TRUE 1 
+#define FALSE 0 
+
+typedef int BOOL ; 
+
+BOOL chkAlpha(char ch)
+{
+    
+    if((ch >= 'A'  && ch <= 'Z'))
+    {
+        return TRUE ; 
+    }
+
+    else if (ch >= 'a'  && ch <= 'z')
+    {
+        return TRUE ; 
+    }
+    
+    else
+    {
+        return FALSE ; 
+    }
+
+}
+
+int main()
+{
+    char cValue = '\0' ; 
+    BOOL bRet = FALSE ; 
+
+    printf("Enter the Charater : ");
+    scanf("%c",&cValue) ;
+
+    bRet = chkAlpha(cValue) ;
+
+    if(bRet == TRUE)
+    {
+        printf("iT IS a Charater") ; 
+    }
+    else
+    {
+        printf("It is Not a Character ");
+    }
+
+    return 0 ; 
+}
