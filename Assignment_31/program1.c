74dfc0af3d6fb737b3bf663775dfe98ed4226374
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-20 12:44:20 +0530

    Add files via upload

--- Assignment_31/program1.c
+++ Assignment_31/program1.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+int countCapital(char *str)
+{
+    int Cnt =0 ;
+
+    if(str == NULL)
+    {
+        return 0 ; 
+    }
+
+    while (*str != '\0')
+    {
+        if(*str>= 'A' && *str <= 'Z')
+        {
+            Cnt++ ; 
+        }
+        str++ ; 
+    }
+    return Cnt ; 
+    
+}
+int main()
+{
+    char arr[20] ; 
+    int iRet = 0 ;
+
+    printf("Enter the String : \n ") ; 
+    scanf("%[^'\n']s",arr) ;
+
+
+    iRet = countCapital(arr) ; 
+
+    printf("%d",iRet) ; 
+
+    return 0 ; 
+}
