61e85f13a3796bfc51d26e35951b9329c01973a1
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-20 13:06:04 +0530

    Add files via upload

---
+++ Assignment_31/program2.c
@@ -0,0 +1,40 @@
+// Write a program which accept string from user and count number of small characters. 
+// Input :   “Marvellous” 
+// Output :  9 
+# include <stdio.h>
+
+int CountSmall(char *str)
+{
+    int iCnt = 0 ; 
+    
+    if(str == NULL)
+    {
+        return 0 ; 
+    }
+
+    while(*str != '\0')
+    {
+        if(*str >= 'a' &&  *str <= 'z')
+        {
+            iCnt++ ; 
+        }
+        str++ ; 
+
+    }
+    return iCnt ; 
+}
+
+int main()
+{
+    char arr[20]; 
+    int iRet = 0 ;
+
+    printf("Enter the String : "); 
+    scanf("%[^\n]s",arr) ; 
+
+    iRet = CountSmall(arr) ; 
+
+    printf("%d",iRet) ; 
+
+    return  0 ; 
+}
