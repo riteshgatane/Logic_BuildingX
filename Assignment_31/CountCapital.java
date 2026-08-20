c60bace48db217c580575bbe495e6fe5c8f3f985
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-20 12:45:17 +0530

    Add files via upload

---
+++ Assignment_31/CountCapital.java
@@ -0,0 +1,46 @@
+//Input : “Marvellous Multi OS”
+
+//Output : 4
+
+import java.util.Scanner;
+
+class DoCapital
+{
+    public static void main(String A[])
+    {
+        int iCount = 0 ; 
+        String str = null ; 
+        int i = 0 ; 
+        Scanner sobj = new Scanner(System.in) ; 
+
+        System.out.println("Enter the String :") ; 
+        str = sobj.nextLine() ;
+
+        str = str.trim() ; 
+        str = str.replaceAll("\\s+" , " ") ; 
+
+        char Arr[] = str.toCharArray() ; 
+
+        for(i = 0 ; i < Arr.length ; i++)
+        {
+            if(Arr[i] == ' ')
+            {
+            if(Arr[i+1] >= 'a' && Arr[i+1] <= 'z')
+            {
+                Arr[i+1] = (char)(Arr[i+1] - 32) ;
+                iCount++ ;  
+            }
+            }
+        }
+
+
+        String output = new String(Arr) ; 
+
+        System.out.println("String of Words with First letter capital is : "+output+"  Number of Capital Letter After Conversion is :"+iCount); ; 
+
+
+        
+
+
+    }
+}
