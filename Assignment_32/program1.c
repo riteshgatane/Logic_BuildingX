309089e628caa297de67de2d71bca200df0fb117
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-27 06:56:03 +0530

    Add files via upload

--- Assignment_32/program1.c
+++ Assignment_32/program1.c
@@ -1,1 +1,33 @@
+/*
+    Converting the letter into Small Letters
+*/
 
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void strlwrx(char *str)
+{
+    int i = 0 ; 
+    int len = strlen(str);
+
+    for(i =0 ; i < len ; i++)
+    {
+       str[i] = tolower(str[i]) ; 
+    }
+}
+
+
+int main()
+{
+    char arr[20] ; 
+
+    printf("Enter String :") ; 
+    scanf("%[^'\n']s", arr); 
+
+    strlwrx(arr);
+
+    printf("Modified String is %s",arr); 
+
+    return 0 ; 
+}
