573305fd3f0f6119ac0992f5a01249d4c3a2feca
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-29 22:57:31 +0530

    Add files via upload

---
+++ Assignment_33/program2.c
@@ -0,0 +1,43 @@
+/*
+program which accept string from user and accept one 
+character. Return frequency of that character. 
+*/
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+int ChkChar(char *str ,char ch )
+{
+    int icnt =0 ;
+    int i = 0 ; 
+    int len = strlen(str);
+    
+    for(i= 0  ; i < len ; i++ )
+    {
+         if(str[i] == ch)
+         {
+            icnt ++;  
+         } 
+    }
+    return icnt ; 
+}
+
+int main()
+{
+    char arr[20] ; 
+    char cValue ; 
+    int iRet =0 ;
+
+    printf("Enter the String : ") ; 
+    scanf("%[^\n]s", arr) ; 
+
+    printf("Enter the Character :  ") ; 
+    scanf(" %c",&cValue) ;  
+
+    iRet = ChkChar(arr,cValue);
+    
+    printf("Number of character in string is : %d", iRet) ;
+    return 0  ; 
+}
