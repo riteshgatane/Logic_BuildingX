8aecd195968e46e2e811eb744f4be6ea25a4c96a
ritesh_gatane <riteshgatane2006@gmail.com>
2026-08-30 07:16:23 +0530

    Add files via upload

--- Assignment_34/program1.c
+++ Assignment_34/program1.c
@@ -1,1 +1,31 @@
+/*
+program which accept string from user and copy the 
+contents of that string into another string. (Implement strcpy() 
+function)
+*/
 
+#include<stdio.h>
+
+void StrcpyX(char *src , char *dest)
+{
+        while (*src != '\0')
+        {
+            *dest = *src ; 
+            src++  ; 
+            dest++ ; 
+        }
+        *dest = '\0' ; 
+}
+
+
+int main()
+{
+    char arr[30] = "Marvellous Multi OS" ; 
+    char brr[30] ; 
+
+    StrcpyX(arr , brr) ; 
+
+    printf("%s\n" , brr) ; 
+
+    return 0 ; 
+}
