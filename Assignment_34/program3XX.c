e9fe4dd9d9293d4e9175792479408853006deb60
ritesh_gatane <riteshgatane2006@gmail.com>
2026-09-02 19:26:47 +0530

    Add files via upload

---
+++ Assignment_34/program3XX.c
@@ -0,0 +1,32 @@
+/*Write a program which accept string from user and copy capital 
+characters of that string into another string. 
+*/
+#include <stdio.h>
+
+void StrCpyCap(char *src , char *dest)
+{
+    while (*src != '\0')
+    {
+        if((*src >= 'A' && *src <= 'Z'))
+        {
+            *dest = *src;
+            dest ++ ; 
+        }
+        src++ ; 
+    }
+    *dest = '\0' ; 
+    
+}
+
+int main()
+{
+    char arr[30] ; 
+    char brr[30] ;
+
+    printf("Enter the String to Seperate the Capital letter :") ; 
+    scanf("%[^\n]s",arr) ; //ISSUE
+    StrCpyCap(arr , brr) ; 
+
+    printf("%s",brr);
+    return 0 ; 
+}
